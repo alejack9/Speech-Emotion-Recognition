6ec63204b91871fd40571e80483402424e817c30
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2022-07-27 16:50:10 +0200

    allow figure saving & kde_plot function

--- Paper/libs/data_visualization.py
+++ Paper/libs/data_visualization.py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
+import logging
 
-def plot_labels_distribution(labels_distribution, one_hot_mapper):
-    _, axes = plt.subplots(1, 3) #, figsize=(30, 10))
+def show_or_save(output_file):
+    if output_file == "":
+        plt.show()
+    else:
+        plt.savefig(output_file, bbox_inches='tight', dpi=300)
+        logging.debug(f"Plot saved in {output_file}")
+        plt.close()
+
+def kde_plot(serie, output_file=""):
+    sns.kdeplot(serie)
+    show_or_save(output_file)
+
+def plot_labels_distribution(labels_distribution, one_hot_mapper, output_file=""):
+    _, axes = plt.subplots(1, 3, figsize=(30, 10))
     titles = ["Complete DataSet's Labels distribution", "Train Set's Labels distribution", "Validation Set's Labels distribution"]
     ds_names = ['complete', 'train', 'val']
     ds_labels_distributions = [
@@ -15,11 +29,12 @@ def plot_labels_distribution(labels_distribution, one_hot_mapper):
     for i, (title, ds_l_dist) in enumerate(zip(titles, ds_labels_distributions)):
         axes[i].set_title(title)
         axes[i].pie(ds_l_dist.values, labels=ds_l_dist.index, autopct='%1.1f%%')
-    plt.show()
 
-def plot_audio_waves(train_ds, one_hot_mapper, rows=3, cols=3):
+    show_or_save(output_file)
+
+def plot_audio_waves(train_ds, one_hot_mapper, rows=3, cols=3, output_file=""):
     n = rows * cols
-    fig, axes = plt.subplots(rows, cols) #, figsize=(12, 15))
+    _, axes = plt.subplots(rows, cols, figsize=(12, 15))
 
     for i, (audio, label) in enumerate(train_ds.take(n)):
      r = i // cols
@@ -29,10 +44,10 @@ def plot_audio_waves(train_ds, one_hot_mapper, rows=3, cols=3):
      label = one_hot_mapper.get(np.array_str(label.numpy()), np.array_str(label.numpy()))
      ax.set_title(label)
 
-    plt.show()
+    show_or_save(output_file)
 
-def plot_speakers_pie(df, rows=2, cols=2):
-    _, axes = plt.subplots(rows, cols) #, figsize=(10, 10))
+def plot_speakers_pie(df, rows=2, cols=2, output_file=""):
+    _, axes = plt.subplots(rows, cols, figsize=(10, 10))
 
     for i, speaker in enumerate(df['speaker'].unique()):
         values = df[df['speaker'] == speaker]['label'].value_counts().sort_index()
@@ -45,4 +60,4 @@ def plot_speakers_pie(df, rows=2, cols=2):
         ax.pie(values.values, labels=values.index,
                autopct=lambda p: '{:.0f}'.format(p * values.values.sum() / 100))
 
-    plt.show()
+    show_or_save(output_file)
