e926b57b8a26072ca72ed21557ce40379e49d51b
Alessandra Boccuto <aless97b@gmail.com>
2022-11-15 13:10:04 +0100

    add paper model preprocessing combinations

--- Paper/hyperparams.py
+++ Paper/hyperparams.py
@@ -84,15 +84,73 @@ ESD_custom_model_combinations = {
 
 paper_model_combinations = {
   'model_factories': [
-    PaperModelFactory(),
+    PaperModelFactory()
   ],
-  'seconds' : [3, 4, 7],
-  'patiences' : [80],
+  'seconds' : [3, 4, 5, 8],
+  'patiences' : [25, 80],
   'dropouts': [0, 0.2, 0.5],
-  'train_val_test_percentages' : [(80, 10, 10)],
+  'train_val_test_percentages' : [(62.5, 20.833, 16.666)],
   'data_operations_factories' : [
     ('crop', lambda _: [
         data_ops.Crop(),
+    ]),
+    ('fade05_crop', lambda total_audio_frames: [
+      data_ops.Fade(total_audio_frames * 0.05, total_audio_frames * 0.05),
+      data_ops.Crop(),
+    ]),
+    ('fade10_crop', lambda total_audio_frames: [
+      data_ops.Fade(total_audio_frames * 0.10, total_audio_frames * 0.10),
+      data_ops.Crop(),
+    ]),
+    ('fade15_crop', lambda total_audio_frames: [
+      data_ops.Fade(total_audio_frames * 0.15, total_audio_frames * 0.15),
+      data_ops.Crop(),
+    ]),
+    ('crop_fade05', lambda total_audio_frames: [
+      data_ops.Crop(),
+      data_ops.Fade(total_audio_frames * 0.05, total_audio_frames * 0.05),
+    ]),
+    ('crop_fade10', lambda total_audio_frames: [
+      data_ops.Crop(),
+      data_ops.Fade(total_audio_frames * 0.10, total_audio_frames * 0.10),
+    ]),
+    ('crop_fade15', lambda total_audio_frames: [
+      data_ops.Crop(),
+      data_ops.Fade(total_audio_frames * 0.15, total_audio_frames * 0.15),
+    ]),
+    ('crop_norm', lambda _: [
+        data_ops.Crop(),
+        data_ops.Normalize()
+    ]),
+    ('fade05_crop_norm', lambda total_audio_frames: [
+      data_ops.Fade(total_audio_frames * 0.05, total_audio_frames * 0.05),
+      data_ops.Crop(),
+      data_ops.Normalize()
+    ]),
+    ('fade10_crop_norm', lambda total_audio_frames: [
+      data_ops.Fade(total_audio_frames * 0.10, total_audio_frames * 0.10),
+      data_ops.Crop(),
+      data_ops.Normalize()
+    ]),
+    ('fade15_crop_norm', lambda total_audio_frames: [
+      data_ops.Fade(total_audio_frames * 0.15, total_audio_frames * 0.15),
+      data_ops.Crop(),
+      data_ops.Normalize()
+    ]),
+    ('crop_fade05_norm', lambda total_audio_frames: [
+      data_ops.Crop(),
+      data_ops.Fade(total_audio_frames * 0.05, total_audio_frames * 0.05),
+      data_ops.Normalize()
+    ]),
+    ('crop_fade10_norm', lambda total_audio_frames: [
+      data_ops.Crop(),
+      data_ops.Fade(total_audio_frames * 0.10, total_audio_frames * 0.10),
+      data_ops.Normalize()
+    ]),
+    ('crop_fade15_norm', lambda total_audio_frames: [
+      data_ops.Crop(),
+      data_ops.Fade(total_audio_frames * 0.15, total_audio_frames * 0.15),
+      data_ops.Normalize()
     ]),
   ]
 }
